af06858da6fa265f25c37d7ab40377e36f9b6e41
all0cer <italoalmeinasc@gmail.com>
2021-01-18 10:51:13 -0300

    Cadastro com Tratamentos de erros

--- hosp/register.py
+++ hosp/register.py
@@ -1,11 +1,16 @@
 def registro(name="", idade=0, local=""):
- while True:
-        try:
-                name = str(input("Digite seu nome: ")).strip().split()
-                idade = int(input("Digite a sua idade: "))
-                local = str(input("Digite o nome de sua cidade: ")).strip().split()
-        except (ValueError, AttributeError):
-                print("Erro. digite algo válido")
-                continue
-        break
- return name, idade, local
+        while name.isalpha() == False:
+                name = input("Digite seu nome: ")
+                if name.isalpha():
+                        name.isalpha() == True
+                else:
+                        print("Por favor digite um nome válido.(Não deve conter espaços)")
+        while True:
+                try:
+                        idade = int(input("Digite a sua idade: "))
+                        break
+                except (ValueError, TypeError):
+                        print ("Digite apenas números inteiros")
+                        
+        local = str(input("Digite o nome de sua cidade: ")).strip().split()
+        return name, idade, local
