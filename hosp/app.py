a0682b9cb7f911ae233743176a4012303cd424bb
all0cer <italoalmeinasc@gmail.com>
2021-02-02 16:46:13 -0300

    Exclusão de variáveis desnecessárias

--- hosp/app.py
+++ hosp/app.py
@@ -5,7 +5,7 @@ cadastro = [register.registro()]
 for item in cadastro:
     print (f"Nome:{item[0]}")
     print (f"Idade:{item[1]}")
-    print (f"Local:{item[2]}")
+    print (f"Sexo:{item[2]}")
 
 cadastro_historico = register.historico()
 
