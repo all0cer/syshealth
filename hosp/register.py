031c8687f2a361b77526f112bad8de6101c5d360
all0cer <italoalmeinasc@gmail.com>
2021-02-02 16:46:46 -0300

    Ajuste nos valores com tratamentos de erros

--- hosp/register.py
+++ hosp/register.py
@@ -1,27 +1,29 @@
-def registro(name="", idade=0, local=""):
-        while name.isalpha() == False:
-                name = input("Digite seu nome: ")
-                if name.isalpha():
-                        break
 
+def registro(name="", idade=0, sexo=""):
+        while True:
+                name = input("Digite seu nome: ")
+                if all(l.isspace() or l.isalpha() for l in name):
+                        break
                 else:
-                        print("Por favor digite um nome válido.(Não deve conter espaços)")
+                        print(f'Por favor digite um nome válido')
         while True:
                 try:
                         idade = int(input("Digite a sua idade: "))
                         break
                 except (ValueError, TypeError):
                         print ("Digite apenas números inteiros")
-        while True:
-                try:
-                        local = input("Digite o nome de sua cidade: ")
-                        if local.isalpha():
-                                break
-                except (ValueError, TypeError):
-                        print("Por favor, digite um local válido.(Não deve conter espaços")
-        return name, idade, local
+        while True: 
+               sexo = input("Digite o sexo(F/M): ").upper()
+               if sexo == 'F':
+                break
+               elif sexo == 'M':
+                break        
+               else:
+                       print(f'Digite F para feminino ou M para masculino')      
+        return name, idade, sexo
 
 
 def historico(historia="",sintomas=""):
         historia = str(input("Digite o histórico de vida do paciente: "))
         sintomas = str(input("Digite os sintomas do paciente: "))
+        medicamentos = str(input("Digite o medicamento a ser usado e a dosagem: "))
